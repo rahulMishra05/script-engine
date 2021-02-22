0a3f9732c616fb57c091c2762c686ed325f4a3cc
Rahul Mishra <rahulmishra102000@gmail.com>
2021-02-22 18:36:49 +0530

    Basic template of the script created

--- script_engine.sh
+++ script_engine.sh
@@ -1,3 +1,38 @@
 #!/usr/bin/bash
 
 echo "Script Engine"
+
+echo "list of categories. Please select any option"
+
+choise1="Shell Python Networking Cryptography General"
+
+select option1 in $choise1;
+do
+	if [ $REPLY = 1 ];
+then 
+	echo "You selected shell script"
+	break
+
+elif [ $REPLY = 2 ]
+then 
+	echo "You selected python script"
+	break
+
+elif [ $REPLY = 3 ]
+then
+	echo "You selected Networking script"
+	break
+
+elif [ $REPLY = 4 ]
+then
+	echo "You selected Cryptography"
+	break
+
+elif [ $REPLY = 5 ]
+then
+	echo "You selected General"
+	break
+else
+	echo "Invaild choise, please choose run the script again"
+fi
+done
