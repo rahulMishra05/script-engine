3139f1946f2f0427f13b8e332f17f63f53d10ea5
Rahul Mishra <rahulmishra102000@gmail.com>
2021-03-24 06:19:49 +0530

    Changes done in script, in order to execute different shell scripts.

---
+++ modules/shell.sh
@@ -0,0 +1,27 @@
+#/usr/bin/bash
+
+echo "[+] List of Shell Scripts. Please select any option:"
+
+choise="DNS-Lookup File-Encrypter-Decrypter Password-Generator"
+
+select option in $choise;
+do
+    if [ $REPLY = 1 ];
+then 
+    echo "[+] Executing DNS Lookup script..."
+    bash ../shell-scripts/dns-lookup-host/script.sh
+    break
+elif [ $REPLY = 2 ]
+then
+    echo "[+] Executing File encrypter decrypter script..."
+    bash ../shell-scripts/file-encrypter-decrypter/encrypt.sh 
+    break
+elif [ $REPLY = 3 ]
+then 
+    echo "[+] Executing Password generator script..."
+    bash ../shell-scripts/password-generator-shell-script/passwordGenerator.sh
+    break
+else
+    echo "[!] Invalid option selected."
+fi
+done
