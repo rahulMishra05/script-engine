b25ac81dcca0b444687223b1d238662f3496910c
Rahul Mishra <rahulmishra102000@gmail.com>
2021-03-26 06:32:38 +0530

    Shell Script to execute python scripts completed. Now we can use this script to execute all python scripts from one place.

---
+++ modules/python.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+echo "[+] List of all Python Scripts. Please select any option:"
+
+choise="ARP-Spoofing Password-Sniffer Port-Scanner Vulnerability-Scanner"
+
+select option in $choise;
+do
+    if [ $REPLY = 1 ];
+then 
+    echo "[+] Executing ARP spoofing script..."
+    python3 ../python-scripts/ARP-spoofing/arp_spoofer.py 
+    break
+elif [ $REPLY = 2 ]
+then 
+    echo "[+] Executing Password sniffer script..."
+    python3 ../python-scripts/password-sniffer/password_sniffer.py 
+    break
+elif [ $REPLY = 3 ]
+then 
+    echo "[+] Executing Port scanner script..."
+    python3 ../python-scripts/port-scanner/port_scanner.py
+    break
+elif [ $REPLY = 4 ]
+then 
+    echo "[+] Executing Vulnerability scanner script..."
+    python3 ../python-scripts/vulnerability-scanner/vulnerability_scanner.py
+    break
+else
+    echo "[!] Invalid option selected."
+fi
+done
+
