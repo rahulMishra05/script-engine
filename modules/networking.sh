7702535ac5904e4ec755f518b9c2e7618940f48f
Rahul Mishra <rahulmishra102000@gmail.com>
2021-03-27 09:10:29 +0530

    Shell script to execute all the networking scripts is completed.

---
+++ modules/networking.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+echo "[+] List of all Networking Scripts. Please select any option:"
+
+choise="ARP-Spoofing DNS-Lookup Port-Scanner"
+
+select option in $choise;
+do
+    if [ $REPLY = 1 ];
+then 
+    echo "[+] Executing ARP spoofing script..."
+    python3 ../networking-scripts/ARP-spoofing/arp_spoofer.py
+    break
+elif [ $REPLY = 2 ]
+then 
+    echo "[+] Executing DNS Lookup script..."
+    bash ../networking-scripts/dns-lookup-host/script.sh
+    break
+elif [ $REPLY = 3 ]
+then 
+    echo "[+] Executing Port scanner script..."
+    python3 ../networking-scripts/port-scanner/port_scanner.py 
+    break
+else
+    echo "[!] Invalid option selected."
+fi
+done
