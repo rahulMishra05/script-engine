6a8cdf9b5c3e3ab34ccc9431a34f6c4dce38015d
Rahul Mishra <rahulmishra102000@gmail.com>
2021-03-29 08:22:54 +0530

    Shell script to execute general scripts from one place completed.

---
+++ modules/general.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+echo "[+] List of General Scripts. Please select any option:"
+
+choise="Password-Generator"
+
+select option in $choise;
+do
+    if [ $REPLY = 1 ];
+then 
+    echo "[+] Executing Password generator script..."
+    bash ../general-scripts/password-generator-shell-script/passwordGenerator.sh
+    break 
+else
+    echo "[!] Invalid option selected."
+fi 
+done
