ea330396e8df4b11fe009d027be700702c8ca13d
Rahul Mishra <rahulmishra102000@gmail.com>
2021-03-30 21:55:59 +0530

    Command to execute all python scripts changed in this shell script.

--- python.sh
+++ python.sh
@@ -9,22 +9,22 @@ do
     if [ $REPLY = 1 ];
 then 
     echo "[+] Executing ARP spoofing script..."
-    python3 ../python-scripts/ARP-spoofing/arp_spoofer.py 
+    python3 python-scripts/ARP-spoofing/arp_spoofer.py 
     break
 elif [ $REPLY = 2 ]
 then 
     echo "[+] Executing Password sniffer script..."
-    python3 ../python-scripts/password-sniffer/password_sniffer.py 
+    python3 python-scripts/password-sniffer/password_sniffer.py 
     break
 elif [ $REPLY = 3 ]
 then 
     echo "[+] Executing Port scanner script..."
-    python3 ../python-scripts/port-scanner/port_scanner.py
+    python3 python-scripts/port-scanner/port_scanner.py
     break
 elif [ $REPLY = 4 ]
 then 
     echo "[+] Executing Vulnerability scanner script..."
-    python3 ../python-scripts/vulnerability-scanner/vulnerability_scanner.py
+    python3 python-scripts/vulnerability-scanner/vulnerability_scanner.py
     break
 else
     echo "[!] Invalid option selected."
