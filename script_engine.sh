be2c90e689da70d5a586d17e3a1240148b50269f
Rahul Mishra <rahulmishra102000@gmail.com>
2021-03-30 21:59:43 +0530

    Main script changed in order to execute shell scripts that will help us to execute all the other scripts present in different categories.

--- script_engine.sh
+++ script_engine.sh
@@ -11,36 +11,36 @@ do
 	if [ $REPLY = 1 ];
 then 
 	echo "You selected shell script"
-	./modules/shell.sh
+	./shell.sh
 	break
 
 elif [ $REPLY = 2 ]
 then 
 	echo "You selected python script"
-	./modules/python.sh
+	./python.sh
 	break
 
 elif [ $REPLY = 3 ]
 then
 	echo "You selected Networking script"
-	./modules/networking.sh
+	./networking.sh
  	break
 
 elif [ $REPLY = 4 ]
 then
 	echo "You selected Cryptography"
-	./modules/cryptography.sh
+	./cryptography.sh
 	break
 
 elif [ $REPLY = 5 ]
 then
 	echo "You selected General"
-	./modules/general.sh
+	./general.sh
 	break
 elif [ $REPLY = 6 ]
 then 
 	echo "You selected Exploitation"
-	./modules/exploitation.sh
+	./exploitation.sh
 	break 
 else
 	echo "Invaild choise, please choose run the script again"
