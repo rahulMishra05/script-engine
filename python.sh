4eac06725f160198d7f5ec2b8099342ba386dd7e
Rahul Mishra <rahulmishra102000@gmail.com>
2021-04-07 08:45:08 +0530

    Changes made in the script that can execute all the python scripts from one place in oreder to execute wifi-scanner script from it.

--- python.sh
+++ python.sh
@@ -2,7 +2,7 @@
 
 echo "[+] List of all Python Scripts. Please select any option:"
 
-choise="ARP-Spoofing Password-Sniffer Port-Scanner Vulnerability-Scanner SSH-Brute-Force"
+choise="ARP-Spoofing Password-Sniffer Port-Scanner Vulnerability-Scanner SSH-Brute-Force WiFi-Scanner"
 
 select option in $choise;
 do
@@ -31,6 +31,13 @@ then
     echo "[+] Executing SSH brute force script..."
     python3 python-scripts/ssh-brute-force/ssh_brute_force.py
     break 
+elif [ $REPLY = 6 ]
+then 
+    echo "[+] Executing Wifi scanner script..."
+    echo "[+] This script needs sudo privilages!!"
+    echo ""
+    sudo python3 python-scripts/wifi-scanner/wifi_scanner.py
+    break
 else
     echo "[!] Invalid option selected."
 fi
