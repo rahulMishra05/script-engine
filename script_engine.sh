2b3b6a6a6610cbe8527eafc2753e727525fab29b
Rahul Mishra <rahulmishra102000@gmail.com>
2021-04-23 10:16:14 +0530

    ASCII banner added in main script.

--- script_engine.sh
+++ script_engine.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/bash
 
-echo "Script Engine"
+cat << "EOF"
+  ___         _      _     ___           _          
+ / __| __ _ _(_)_ __| |_  | __|_ _  __ _(_)_ _  ___ 
+ \__ \/ _| '_| | '_ \  _| | _|| ' \/ _` | | ' \/ -_)
+ |___/\__|_| |_| .__/\__| |___|_||_\__, |_|_||_\___|
+               |_|                 |___/            
+
+EOF
 
 echo "list of categories. Please select any option"
 
