5e2695c6cb5d667aa0aad3b7a23e96416aa5c431
Rahul Mishra <rahulmishra102000@gmail.com>
2021-04-23 10:29:28 +0530

    ASCII banner added in cryptography script file.

--- cryptography.sh
+++ cryptography.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/bash
 
+cat << "EOF"
+   ___               _                           _          ___         _      _      
+  / __|_ _ _  _ _ __| |_ ___  __ _ _ _ __ _ _ __| |_ _  _  / __| __ _ _(_)_ __| |_ ___
+ | (__| '_| || | '_ \  _/ _ \/ _` | '_/ _` | '_ \ ' \ || | \__ \/ _| '_| | '_ \  _(_-<
+  \___|_|  \_, | .__/\__\___/\__, |_| \__,_| .__/_||_\_, | |___/\__|_| |_| .__/\__/__/
+           |__/|_|           |___/         |_|       |__/                |_|          
+
+EOF
+
 echo "[+] List of Cryptographic Scripts. Please select any option: "
 
 choise_crypto="File-Encrypter-Decrypter Password-Generator SSH-Brute-Force"
