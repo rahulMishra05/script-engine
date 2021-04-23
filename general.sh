2565a1548079383df258635f120b98575575873a
Rahul Mishra <rahulmishra102000@gmail.com>
2021-04-23 10:35:44 +0530

    ASCII banner added in general script file.

--- general.sh
+++ general.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/bash
 
+cat << "EOF"
+   ___                       _   ___         _      _      
+  / __|___ _ _  ___ _ _ __ _| | / __| __ _ _(_)_ __| |_ ___
+ | (_ / -_) ' \/ -_) '_/ _` | | \__ \/ _| '_| | '_ \  _(_-<
+  \___\___|_||_\___|_| \__,_|_| |___/\__|_| |_| .__/\__/__/
+                                              |_|          
+
+EOF
+
 echo "[+] List of General Scripts. Please select any option:"
 
 choise="Password-Generator"
